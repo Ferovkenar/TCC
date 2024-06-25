9fbf2da8d3ea2c35f3c0123edb8ea6d4fc3adb92
Ferovkenar <119982780+Ferovkenar@users.noreply.github.com>
2024-06-25 00:17:51 -0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,11 @@
-const mysql = require('mysql2');
-const dotenv = require('dotenv');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-dotenv.config();
-
-const pool = mysql.createPool({
+const db = mysql.createPool({
     host: 192.168.100.70,
     user: root,
     password: Ferovkenar132@,
     database: user_auth
 });
 
-module.exports = pool.promise();
+module.exports = db;

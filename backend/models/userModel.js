4c804d4ba6ec3c9ff4d55e55e4e04c71c7bf0b13
Ferovkenar <119982780+Ferovkenar@users.noreply.github.com>
2024-06-25 00:13:33 -0300

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,15 +1,16 @@
 const db = require('../config/db');
-const bcrypt = require('bcryptjs');
 
-const User = {
-    create: async (user) => {
-        const hashedPassword = await bcrypt.hash(user.password, 10);
-        return db.execute('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', 
-                          [user.username, user.email, hashedPassword]);
-    },
-    findByEmail: (email) => {
-        return db.execute('SELECT * FROM users WHERE email = ?', [email]);
+class User {
+    static async create({ username, email, password }) {
+        const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+        return db.execute(query, [username, email, password]);
     }
-};
+
+    static async findByEmail(email) {
+        const query = 'SELECT * FROM users WHERE email = ?';
+        const [rows] = await db.execute(query, [email]);
+        return rows;
+    }
+}
 
 module.exports = User;

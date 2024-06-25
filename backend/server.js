9e48a9962e1a06e8d5786c2ec8bb3270b7c5b901
Ferovkenar <119982780+Ferovkenar@users.noreply.github.com>
2024-06-25 00:11:28 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/authRoutes.js');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
